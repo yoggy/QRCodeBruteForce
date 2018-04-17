bfbafee876411da8f7434a09cec03053806f5d7a
yoggy <yoggy0@gmail.com>
2018-04-17 19:22:28 +0900

    ...

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,7 @@
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// for cordova
+
 var app = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -10,18 +14,17 @@ var app = {
 
 app.initialize();
 
+/////////////////////////////////////////////////////////////////////////////////////////
 
-var w = 400;
-var h = 400;
-
-var initial_url = "http://example.com/";
+var w = 300;
+var h = 300;
 
 // qrcode.js https://davidshimjs.github.io/qrcodejs/
 var qrcode = new QRCode(
     document.getElementById("qrcode-container"),
     {
         id: "qrcode",
-        text: initial_url,
+        text: "",
         width: w,
         height: h,
         colorDark : "#000000",
@@ -30,17 +33,39 @@ var qrcode = new QRCode(
     }
 );
 
+function qrcode_update() {
+    qrcode.clear();
+    qrcode.makeCode(text_input_component.text);
+}
+
 // vue.js https://jp.vuejs.org/
-var url_input_component = new Vue({
-    el: '#url_input_component',
+var text_input_component = new Vue({
+    el: '#text_input_component',
     data: {
-        url : initial_url
+        text : ""
     },
     methods: {
         on_update : function () {
-            console.log("on_update()");
-            qrcode.clear();
-            qrcode.makeCode(this.url);
         }
     }
 });
+
+function nfs(n, d) {
+    const padding = "0".repeat(d);
+    const msg = (padding + n).slice(-d);
+    return msg;
+}
+
+// 
+setInterval(function() {
+    const d = new Date();
+    const msg = 
+            "" 
+            + nfs(d.getFullYear(), 4)
+            + nfs(d.getMonth(), 2)
+            + nfs(d.getDate(), 2)
+            + nfs(Math.floor(Math.random() * Math.floor(999999)), 6)
+            + nfs(Math.floor(Math.random() * Math.floor(9999)), 4)            
+    text_input_component.text = msg;
+    qrcode_update();
+}, 50);
